8fe4c5c3f9b52f4eae8aa2e8361956dc736ab6ee
Natalia Alvarez <03nataliaalvarez07@gmail.com>
2023-04-04 12:43:41 -0400

    finished polybius

--- src/polybius.js
+++ src/polybius.js
@@ -61,6 +61,11 @@ const polybiusModule = (function () {
     55: "z",
   };
 
+  const numbers = [
+    11, 12, 13, 14, 15, 21, 22, 23, 24, 25, 31, 32, 33, 34, 35, 41, 42, 43, 44,
+    45, 51, 52, 53, 54, 55,
+  ];
+
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
   function polybius(input, encode = true) {
@@ -73,30 +78,52 @@ const polybiusModule = (function () {
     //leave spaces as is
     if (encode === true) {
       for (let i = 0; i < input.length; i++) {
-        //loop through encode object and add each number to string
-        for (let letter in polybiusSquareEncode) {
-          // console.log(polybiusSquareEncode[letter]);
-          if (input[i] === letter) {
-            result += polybiusSquareEncode[input[i]];
+        const letter = input[i];
+        //keep spaces as is
+        if (alphabet.includes(letter)) {
+          //loop through encode object and add each number to string
+          for (let letter in polybiusSquareEncode) {
+            // console.log(polybiusSquareEncode[letter]);
+            if (input[i] === letter) {
+              result += polybiusSquareEncode[input[i]];
+            }
           }
+        } else {
+          result += letter;
         }
       }
     }
 
     //decoding
-    //leave spaces as is
-    if (encode === false) {
-      //if string is odd return false
-      if (input.length / 2 != 0) return false;
+
+    if (!encode) {
       //group into twos
-      let groupOfTwos = input.match(/..?/g);
+      let spacesAdded = input.replace(" ", 65);
+      const groupOfTwos = spacesAdded.split(/(?=(?:..)*$)/);
       console.log(groupOfTwos);
 
-      //loop through decode object
+      //if string is odd return false
+      if (spacesAdded.length % 2 !== 0) return false;
+
       //loop through groups array, if input matches num add it to results array
+      for (let j = 0; j < groupOfTwos.length; j++) {
+        const number = groupOfTwos[j];
+        console.log(numbers.some((el) => el == number));
+
+        //leave spaces
+        if (numbers.some((el) => el == number) === true) {
+          //loop through decode object
+          for (let num in polybiusSquareDecode) {
+            if (num == groupOfTwos[j]) {
+              result += polybiusSquareDecode[groupOfTwos[j]];
+            }
+          }
+        } else {
+          result += " ";
+        }
+      }
     }
 
-    console.log(result);
     return result;
   }
 
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,48 @@
-// Write your tests here!
+const expect = require("chai").expect;
+const { polybius } = require("../src/polybius");
+
+describe("encoding", () => {
+  it("translates letters i and j to 42", () => {
+    const input = "ij";
+    const expected = "4242";
+    const actual = polybius(input);
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("ignores capital letters", () => {
+    const input = "Hello";
+    const expected = "3251131343";
+    const actual = polybius(input);
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("respects spaces when encoding", () => {
+    const input = "Hi there";
+    const expected = "3242 4432512451";
+    const actual = polybius(input);
+
+    expect(actual).to.equal(expected);
+  });
+});
+
+describe("decoding", () => {
+  it("respects spaces when decoding", () => {
+    const input = "3242 4432512451";
+    const encode = false;
+    const expected = "h(i/j) there";
+    const actual = polybius(input, encode);
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("it translates 42 to (i/j)", () => {
+    const input = "42";
+    const encode = false;
+    const expected = "(i/j)";
+    const actual = polybius(input, encode);
+
+    expect(actual).to.equal(expected);
+  });
+});
