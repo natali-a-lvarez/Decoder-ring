958216ae74dc093d56cc321d1ffe35952a03d102
Natalia Alvarez <03nataliaalvarez07@gmail.com>
2023-04-05 12:16:24 -0400

    finished substitution algorithim and tests

--- src/substitution.js
+++ src/substitution.js
@@ -4,10 +4,44 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
+  let regularAlphabet = "abcdefghijklmnopqrstuvwxyz";
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    //make input lowercase
+    input = input.toLowerCase();
+
+    //make sure alphabet it unique, exists, and is 26 characters if not return false
+    const setAlpha = new Set(alphabet);
+    if (!alphabet || alphabet.length != 26 || setAlpha.size !== alphabet.length)
+      return false;
+
+    // make both alphabets into arrayss
+    regularAlphabet = [...regularAlphabet];
+    alphabet = [...alphabet];
+
+    // make an object that has the regual alphabet as keys and the new one as values
+    let result = {};
+    result = Object.assign(
+      ...regularAlphabet.map((l, i) => ({ [l]: alphabet[i] }))
+    );
+
+    //helper function for decoding
+    function getKeyByValue(object, value) {
+      return Object.keys(object).find((key) => object[key] === value);
+    }
+
+    // Encode
+    let encodedWord = "";
+    for (let letter of input) {
+      if (letter === " ") {
+        encodedWord += letter;
+      } else if (encode) {
+        encodedWord += result[letter];
+      } else {
+        encodedWord += getKeyByValue(result, letter);
+      }
+    }
+    return encodedWord;
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,47 @@
-// Write your tests here!
+const expect = require("chai").expect;
+const { substitution } = require("../src/substitution");
+
+describe("substitution", () => {
+  it("returns false if the given alphabet is not 26 characters long", () => {
+    const input = "hello";
+    const alphabet = "xoyqmcgrukswa";
+    const actual = substitution(input, alphabet);
+
+    expect(actual).to.be.false;
+  });
+
+  it("correctly translates frase based on alphabet given", () => {
+    const input = "hello";
+    const alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+    const actual = substitution(input, alphabet);
+    const expected = "d&ccv";
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("ignores capital letters", () => {
+    const input = "HelLo";
+    const alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+    const actual = substitution(input, alphabet);
+    const expected = "d&ccv";
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("maintains spaces", () => {
+    const input = "Hello there";
+    const alphabet = "$wae&zrdxtfcygvuhbijnokmpl";
+    const actual = substitution(input, alphabet);
+    const expected = "d&ccv jd&b&";
+
+    expect(actual).to.equal(expected);
+  });
+
+  it("maintains false if the given alphabet is not unique", () => {
+    const input = "Hello there";
+    const alphabet = "$wae&zrdxtacygvuhbijnokmpl";
+    const actual = substitution(input, alphabet);
+
+    expect(actual).to.be.false;
+  });
+});
